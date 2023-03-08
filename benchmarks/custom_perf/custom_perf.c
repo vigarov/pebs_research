cf94f7477f42c1adacb4b952b67606158221f217
Victor <vesbru@yahoo.fr>
2023-03-07 17:52:00 -0800

    modularized error handling, added -m option, correctly parsing sample

--- benchmarks/custom_perf/custom_perf.c
+++ benchmarks/custom_perf/custom_perf.c
@@ -21,10 +21,23 @@
 #include <sys/ioctl.h>
 #include <error.h>
 
-#define DEBUG 1
+#define DEBUG 0
 
 #define likely(x) __builtin_expect((x),1)
 #define unlikely(x) __builtin_expect((x),0)
+
+#define err(error_string) do{ unsigned int error_line = __LINE__;                                     \
+    char line_nr[10] = {0};                                                                          \
+    snprintf(line_nr,9,"%d",error_line);                                                              \
+    const char* user_error_string = error_string;                                                      \
+    const size_t f_len = strlen(__func__), l_len = strlen(line_nr), us_len = strlen(user_error_string), \
+    nb_calloc_chars = f_len + l_len + us_len + 3;                                                        \
+    char *final_str = calloc(nb_calloc_chars,sizeof(char));                                               \
+    if(final_str == NULL){printf("Error allocating memory for... error string. Exiting."); exit(-1);}      \
+    strncat(final_str,__func__,nb_calloc_chars); strncat(final_str,":",nb_calloc_chars-f_len);              \
+    strncat(final_str,line_nr,nb_calloc_chars-f_len-1); strncat(final_str," ",nb_calloc_chars-f_len-1-l_len);\
+    strncat(final_str,user_error_string,nb_calloc_chars-f_len-1-l_len-1);                                     \
+    perror(final_str); free(final_str); }while(0)
 
 const char *argp_program_version = "custom_perf 1.0";
 
@@ -57,19 +70,26 @@ error_t parse_opt(int key, char *arg, struct argp_state *state) {
         case 'c':
             arguments->counter = strtoul(arg,NULL,10);
             if (arguments->counter <= 0) {
-                argp_error(state, "Counter must be a positive integer");
+                argp_error(state, "Counter must be a positive integer\n");
             }
             break;
-        case 'm':
-            arguments->mmap_size = strtoul(arg,NULL,10);
+        case 'm': {
+            char *end = NULL;
+            arguments->mmap_size = strtoul(arg, &end, 10);
+            if(end != NULL){
+                if(*end == 'K' || *end == 'k')arguments->mmap_size*=1024;
+                else if(*end == 'M' || *end == 'm') arguments->mmap_size*=1024*1024;
+                else if(*end == 'G' || *end == 'g') arguments->mmap_size*=1024*1024*1024;
+                else argp_error(state,"Incorrect letter for mmap size");
+            }
             if (arguments->mmap_size <= 0) {
-                argp_error(state, "Mmap size must be a positive integer");
-            }
-            else if(__builtin_popcount(arguments->mmap_size) != 1){
+                argp_error(state, "Mmap size must be a positive integer\n");
+            } else if (__builtin_popcount(arguments->mmap_size) != 1) {
                 // Not a power of 2 --> Approximate to the closest power of 2
-                arguments->mmap_size = (8*sizeof(arguments->mmap_size)) - __builtin_clzl(arguments->mmap_size-1);
+                arguments->mmap_size = (8 * sizeof(arguments->mmap_size)) - __builtin_clzl(arguments->mmap_size - 1);
             }
             break;
+        }
         case ARGP_KEY_INIT:
             break; // Do nothing
         case ARGP_KEY_ARG: {
@@ -124,8 +144,15 @@ perf_event_open(struct perf_event_attr *hw_event, pid_t pid,
 }
 
 #define GET_PERF_ATTR(var_name,config_struct,args_p_var_name) struct perf_event_attr var_name = { .type=PERF_TYPE_RAW, .size=sizeof(struct perf_event_attr), \
-.config=(config_struct), .sample_period=(args_p_var_name)->counter, .sample_type=PERF_SAMPLE_TIME|PERF_SAMPLE_ADDR|PERF_SAMPLE_PHYS_ADDR|PERF_SAMPLE_TID|PERF_SAMPLE_IP, \
-.read_format=PERF_FORMAT_TOTAL_TIME_RUNNING, .disabled=1, .inherit=0, .exclude_kernel=1, .exclude_hv = 1 ,.freq=0, .enable_on_exec=1, .precise_ip=2,}
+.config=(config_struct), .sample_period=(args_p_var_name)->counter,.sample_type=PERF_SAMPLE_IP|PERF_SAMPLE_TIME|PERF_SAMPLE_ADDR|PERF_SAMPLE_PHYS_ADDR, \
+.read_format=PERF_FORMAT_TOTAL_TIME_RUNNING, .disabled=1, .exclude_kernel=1, .exclude_hv = 1 ,.freq=0, .enable_on_exec=1, .precise_ip=2}
+
+struct perf_sample{ //TODO: update as ATTR above changes
+    uint64_t   ip;
+    uint64_t   time;
+    uint64_t   addr;
+    uint64_t   phys_addr;
+};
 
 enum event_type{NONE_EVENT=-1,LOAD,STORE,NM_EVENTS};
 
@@ -133,18 +160,18 @@ enum event_type{NONE_EVENT=-1,LOAD,STORE,NM_EVENTS};
 static inline void switch_events(int loads_event_fd, int stores_event_fd, unsigned long int signal) {
     int error = ioctl(loads_event_fd, signal);
     if(error){
-        printf("Failed to stop load event: %s", strerror(error));
+        err("Failed to stop load event\n");
     }
     error = ioctl(stores_event_fd,signal);
     if(error){
-        printf("Failed to stop load event: %s", strerror(error));
+        err("Failed to stop load event");
     }
 }
 
 #define ENABLE_PEBS_EVENTS(load_fd,store_fd) switch_events((load_fd),(store_fd),PERF_EVENT_IOC_ENABLE)
 #define DISABLE_PEBS_EVENTS(load_fd,store_fd) switch_events((load_fd),(store_fd),PERF_EVENT_IOC_DISABLE)
 
-
+#define GET_START_OFFSET() (addr_to_use + ((mmap_header->data_offset+at)%mmap_header->data_size))
 
 
 void gather_stats(struct arguments *args) {
@@ -163,141 +190,211 @@ void gather_stats(struct arguments *args) {
    */
     int error = 0;
 
-    //TODO: Usage of wakeup_watermark/events when sample_period is specified??
-    GET_PERF_ATTR(loads_event_arguments,0x81d0,args);
-    GET_PERF_ATTR(stores_event_arguments,0x82d0,args);
-
-    // This process (future parent) will be the one `exec`ing the benchmark --> we want to "pin" the PERF events to its pid, but (potentially) any cpu
-    pid_t curr_pid = getpid();
-
-    int loads_event_fd = perf_event_open(&loads_event_arguments,curr_pid,-1,-1,0);
-    if(loads_event_fd == -1) goto free_args;
-    int stores_event_fd = perf_event_open(&stores_event_arguments,curr_pid,-1,-1,0);
-    if(stores_event_fd == -1) goto close_load_fd;
-
-    //umask = config:8-15 ; event = config:0-7
-    // mem_inst_retired.all_loads -> cpu/(null)=0x1e8483,umask=0x81,event=0xd0/
-    // mem_inst_retired.all_stores -> cpu/(null)=0x1e8483,umask=0x82,event=0xd0/
-    const size_t mmap_size = sizeof(struct perf_event_mmap_page) + (args->mmap_size);
-    // Must 1 + 2^n pages big
-    void* load_mmap_addr_start = mmap(NULL, mmap_size, PROT_READ | PROT_WRITE, MAP_SHARED, loads_event_fd, 0);
-    if(load_mmap_addr_start == MAP_FAILED){
-        printf("Failed to create load map");
-        goto close_store_fd;
+    int pipefd[2];
+    if (pipe(pipefd) == -1) {
+        err("Couldn't create pipe");
+        goto free_args;
     }
-    void* store_mmap_addr_start = mmap(NULL, mmap_size, PROT_READ | PROT_WRITE, MAP_SHARED, stores_event_fd, 0);
-    if(store_mmap_addr_start == MAP_FAILED){
-        printf("Failed to create store map");
-        goto unmap_load;
-    }
+
     #if DEBUG
-    printf("Forking");
+    printf("Forking\n");
     #endif
     pid_t child_pid = fork();
 
     if(child_pid==-1){
-        printf("Failed to fork");
-        goto unmap_store;
+        err("Failed to fork\n");
+        goto close_pipe;//unmap_store;
     }
     else if(child_pid == 0){
         //Child
-        pause(); // Wait for CONT signal;
-        printf("Child Process: Received CONT sig, continuing");
+        if(close(pipefd[1])==-1)err("Child couldn't close W side of pipe");
+        char buf;
+        if(unlikely(read(pipefd[0],&buf,1)!=0)){
+            printf("Child didn't receive EOF, but rather char w/ `int` value of %d. Still continuing.",(unsigned)buf);
+        }
+        #if DEBUG
+        printf("Child Process received parent signal: continuing\n");
+        #endif
+        close(pipefd[0]);
         error = execvp(args->executable_path,args->executable_args);
         if(error){
-            printf("Child Process: Failed to exec: %s", strerror(errno));
+            err("Child Process: Failed to exec");
             error = kill(getppid(),SIGTERM);
             if(error){
-                printf("Child Process: Could not terminate parent: %s", strerror(errno));
+                err("Child Process: Could not terminate parent");
             }
             exit(error);
         }
     }
     else{
+        if(close(pipefd[0])==-1)err("Parent couldn't close R side of pipe");
+        // Set up the PEBS events
+        //TODO: Usage of wakeup_watermark/events when sample_period is specified??
+        GET_PERF_ATTR(loads_event_arguments,0x81d0,args);
+        GET_PERF_ATTR(stores_event_arguments,0x82d0,args);
+
+        int loads_event_fd = perf_event_open(&loads_event_arguments,child_pid,-1,-1,0);
+        if(loads_event_fd == -1) goto terminate_child;
+        int stores_event_fd = perf_event_open(&stores_event_arguments,child_pid,-1,-1,0);
+        if(stores_event_fd == -1) goto close_load_fd;
+
+        //umask = config:8-15 ; event = config:0-7
+        // mem_inst_retired.all_loads -> cpu/(null)=0x1e8483,umask=0x81,event=0xd0/
+        // mem_inst_retired.all_stores -> cpu/(null)=0x1e8483,umask=0x82,event=0xd0/
+        const size_t mmap_size = sizeof(struct perf_event_mmap_page) + (args->mmap_size);
+        // Must 1 + 2^n pages big
+        unsigned char* load_mmap_addr_start = mmap(NULL, mmap_size, PROT_READ | PROT_WRITE, MAP_SHARED, loads_event_fd, 0);
+        if(load_mmap_addr_start == MAP_FAILED){
+            err("Failed to create load map");
+            goto close_store_fd;
+        }
+        unsigned char* store_mmap_addr_start = mmap(NULL, mmap_size, PROT_READ | PROT_WRITE, MAP_SHARED, stores_event_fd, 0);
+        if(store_mmap_addr_start == MAP_FAILED){
+            err("Failed to create store map");
+            goto unmap_load;
+        }
+
         //Parent
-        sleep(3); //Give time for child to set up and pause() and wait for us to set up
         struct pollfd to_poll[NM_EVENTS] = {{.fd=loads_event_fd,.events=POLLIN | POLLERR | POLLHUP},{.fd=stores_event_fd,.events=POLLIN | POLLERR | POLLHUP}};
+            if(unlikely(close(pipefd[1])==-1)) err("Parent finished but couldn't close W side of pipefd "); //Starts child
+        uint64_t l_count = 0, s_count = 0, woken = 0;
         while(1){
-            //Reset revents
-            for(int i=0;i<NM_EVENTS;i++){
-                to_poll[i].revents=0;
-            }
             int read = poll(to_poll,NM_EVENTS,-1);
             if(likely(read > 0)){
                 // One of the fds is ready to be read
                 //Stop the process
                 error = kill(child_pid,SIGSTOP);
                 if(unlikely(error)){
-                    printf("Couldn't pause child process: %s", strerror(errno));
+                    err("Couldn't pause child process");
                 }
                 //Just in case, disable PEBS sampling
-                DISABLE_PEBS_EVENTS(loads_event_fd, stores_event_fd);
+                //DISABLE_PEBS_EVENTS(loads_event_fd, stores_event_fd);
+                woken+=1;
+                #if DEBUG
+                printf("Woke up from read\n");
+                #endif
                 if(read!=1){
-                    printf("More than one event ready at the same time... Only parsing first, losing others");
+                    printf("More than one event ready at the same time... Only parsing first, losing others\n");
                 }
+                uint8_t quit = 0;
                 for(int i = LOAD;i<NM_EVENTS;i++){
                     if(to_poll[i].revents!=0){
                         // Analyze the data
-                        printf("Got data from %d !",(uint8_t)i);
-                        goto terminate_child;
+#if DEBUG
+                        printf("Got revent from %d: %d !\n",(uint8_t)i,to_poll[i].revents);
+#endif
+                        if(to_poll[i].revents != POLL_IN) {
+                            quit = 1;
+                            continue;
+                        }
+                        unsigned char* addr_to_use = ((i == LOAD )? load_mmap_addr_start : store_mmap_addr_start);
+                        struct perf_event_mmap_page* mmap_header = (struct perf_event_mmap_page* )addr_to_use;
+                        size_t at = mmap_header->data_tail;
+                        while(at<mmap_header->data_head){
+                            struct perf_event_header* event_header = (struct perf_event_header*)(addr_to_use + ((mmap_header->data_offset+at)%mmap_header->data_size));
+                            at += sizeof(struct perf_event_header);
+                            if(event_header->type == PERF_RECORD_SAMPLE){
+                                    printf("Different sizes...");
+                                }
+#if DEBUG
+#endif
+                            if(event_header->size != sizeof(struct perf_sample)){
+                                struct perf_sample* sample = (struct perf_sample*)GET_START_OFFSET();
+                                if(i == LOAD) l_count++;
+                                else if(i==STORE) s_count++;
+                                //printf("dsadsad");
+                            }
+#if DEBUG
+                            else{
+                                printf("Got non sample... weird");
+                            }
+#endif
+                            at+=sizeof(struct perf_sample);
+                        }
+                        mmap_header->data_tail = at;
                     }
                 }
+                if(quit) break;
             }
             else if(unlikely(read == 0)){
-                printf("Poll timed out yet no timeout set...");
+                printf("Poll timed out yet no timeout set...\n");
                 break;
             }
             else{
                 // Error
-                printf("Error polling on the file descriptors: %s", strerror(errno));
+                err("Error polling on the file descriptors");
                 break;
             }
+            //Reset revents
+            for(int i=0;i<NM_EVENTS;i++){
+                to_poll[i].revents=0;
+            }
             //Reenable PEBS
-            ENABLE_PEBS_EVENTS(loads_event_fd,stores_event_fd);
+            //ENABLE_PEBS_EVENTS(loads_event_fd,stores_event_fd);
             //Resume the process
             error = kill(child_pid,SIGCONT);
             if(unlikely(error)){
-                printf("Couldn't resume child process: %s", strerror(errno));
+                err("Couldn't resume child process");
             }
         }
 
-        /*while(kill(child_pid,0) == 0) { //while child is alive
-            #if DEBUG
-            printf("Child is alive, waking it up");
-            #endif
-            kill(child_pid, SIGCONT); //Resume child process execution
+        for(enum event_type i = LOAD;i<NM_EVENTS;i++) {
+            unsigned char *addr_to_use = ((i == LOAD) ? load_mmap_addr_start : store_mmap_addr_start);
+            struct perf_event_mmap_page *mmap_header = (struct perf_event_mmap_page *) addr_to_use;
+            size_t at = mmap_header->data_tail;
+            while (at < mmap_header->data_head) {
+                if(i == LOAD) l_count++;
+                else if(i==STORE) s_count++;
+                struct perf_event_header *event_header = (struct perf_event_header *) (addr_to_use +
+                                                                                       ((mmap_header->data_offset +
+                                                                                         at) % mmap_header->data_size));
+                at += sizeof(struct perf_event_header);
+                if (event_header->type == PERF_RECORD_SAMPLE) {
+                    struct perf_sample *sample = (struct perf_sample *) GET_START_OFFSET();
+                    //printf("dsadsad");
+                }
+                at += sizeof(struct perf_sample);
+            }
+            if(at > mmap_header->data_head) at = mmap_header->data_head;
+            mmap_header->data_tail = at;
+        }
 
-        }*/
-
-    }
+        printf("Got load %llu,store %llu, woken %llu",l_count,s_count,woken);
 
 
-    terminate_child:
-    if(kill(child_pid,0) == 0){ //child is still alive
-        kill(child_pid,SIGTERM);
+        unmap_store:
+        error = munmap(store_mmap_addr_start,mmap_size);
+        if(error == -1){
+            printf("Failed to unmap store map\n");
+        }
+        store_mmap_addr_start = NULL;
+        unmap_load:
+        error = munmap(load_mmap_addr_start,mmap_size);
+        if(error == -1){
+            printf("Failed to unmap load map\n");
+        }
+        load_mmap_addr_start = NULL;
+        close_store_fd:
+        error = close(stores_event_fd);
+        if(error==-1){
+            printf("Failed to close store perf event fd\n");
+        }
+        close_load_fd:
+        error = close(loads_event_fd);
+        if(error==-1) {
+            printf("Failed to close loads perf event fd\n");
+        }
+        terminate_child:
+        if(kill(child_pid,0) == 0){ //child is still alive
+            kill(child_pid,SIGTERM);
+        }
+        goto free_args; //Skip close_pipe, as we've already closed some of the parts
     }
-    unmap_store:
-    error = munmap(store_mmap_addr_start,mmap_size);
-    if(error == -1){
-        printf("Failed to unmap store map");
-    }
-    store_mmap_addr_start = NULL;
-    unmap_load:
-    error = munmap(load_mmap_addr_start,mmap_size);
-    if(error == -1){
-        printf("Failed to unmap load map");
-    }
-    load_mmap_addr_start = NULL;
-    close_store_fd:
-    error = close(stores_event_fd);
-    if(error==-1){
-        printf("Failed to close store perf event fd");
-    }
-    close_load_fd:
-    error = close(loads_event_fd);
-    if(error==-1) {
-        printf("Failed to close loads perf event fd");
-    }
+
+
+    close_pipe:
+    if(close(pipefd[0])==-1)err("couldn't close pipefd0");
+    if(close(pipefd[1]) == -1)err("couldn't close pipefd1");
     free_args:
     // free argument memory
     free(args->executable_args); //never null if file specified, as first argument is always path name
@@ -310,17 +407,11 @@ int get_remainder_arguments(struct arguments *arguments, int argc, char **argv) 
 
     //We have argc - exec_pos - 1 for-the-executable arguments
     arguments->num_extra_executable_args = argc - exec_pos - 1;
-    if(arguments->num_extra_executable_args == 0){
-        //No additional executable arguments; our job here is done
-        return 0;
-    }
-    const size_t malloc_size = (arguments->num_extra_executable_args + 2) * sizeof(char*); //+1 for program name, +1 for null ptr at the end
-    arguments->executable_args = malloc(malloc_size);
-    memset(arguments->executable_args,0,malloc_size);
+    arguments->executable_args = calloc(arguments->num_extra_executable_args + 2,sizeof(char*));//+1 for program name, +1 for null ptr at the end
     if(arguments->executable_args == NULL){
        return ENOMEM;
     }
-    for(uint8_t i=0; i<arguments->num_extra_executable_args; i++){ //+1 iteration since the arguments start at the exec name, situated at exec_pos
+    for(uint8_t i=0; i<arguments->num_extra_executable_args+1; i++){ //+1 iteration since the arguments start at the exec name, situated at exec_pos
         arguments->executable_args[i] = argv[i+exec_pos];
     }
     return 0;
@@ -341,7 +432,7 @@ int main(int argc, char* argv[]){
 
     int error = get_remainder_arguments(&arguments,argc,argv);
     if(error){
-        printf("Error parsing program_arguments : %s", strerror(error));
+        err("Error parsing program_arguments");
         return error;
     }
 
