94757fb568889d19268810e477f7bf58b5b98d57
Victor <vesbru@yahoo.fr>
2023-07-03 12:10:53 +0200

    added support for canneal,bodytrack

--- benchmarks/page-replacement-algs/c_rewrite/main.cpp
+++ benchmarks/page-replacement-algs/c_rewrite/main.cpp
@@ -165,7 +165,7 @@ struct Args {
             }
         }
         if(!changed) {
-            const std::vector<std::string> KNOWN_PARSEC_BENCHMARKS = {"ferret","dedup"};
+            const std::vector<std::string> KNOWN_PARSEC_BENCHMARKS = {"ferret","dedup","bodytrack","canneal"};
             for (const auto &bm: KNOWN_PARSEC_BENCHMARKS) {
                 if (mem_trace_path_fs.native().find(bm) != std::string::npos) {
                     bm_name = "parsec/"+bm;
